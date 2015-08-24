512211812262b97b92f809ce12ea2b2692cdd25e
Russell <russell.s.stewart@gmail.com>
2015-08-24 16:20:21 -0700

    Added data download script.

---
+++ get_brainwash_train.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# This scripts downloads the ptb data and unzips it.
+
+DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+cd $DIR
+
+echo "Downloading..."
+
+mkdir -p data && cd data
+wget http://russellsstewart.com/s/reinspect/brainwash_mean.npy
+wget http://datasets.d2.mpi-inf.mpg.de/brainwash/brainwash.tar
+tar xf brainwash.tar
+
+echo "Done."
